ee008e33117aa7e34f447f337a71e518f811dd63
sanyochek58 <sanya.burayev.03@mail.ru>
2025-11-07 15:04:27 +0300

    Реализация задачи 6

--- Task2/app/src/main/java/com/example/task2/MainActivity.kt
+++ Task2/app/src/main/java/com/example/task2/MainActivity.kt
@@ -5,14 +5,27 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.task2.ui.theme.Task2Theme
+import kotlinx.coroutines.sync.Mutex
 
 class MainActivity : ComponentActivity() {
     private val TAG = "MainActivity"
@@ -68,10 +81,64 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+//@Preview(showBackground = true, showSystemUi = true)
+//@Composable
+//fun GreetingPreview() {
+//    Task2Theme {
+//        Greeting("Android")
+//    }
+//}
+
+
+// Задача 6
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    device = "spec:parent=Galaxy Nexus, orientation=portrait"
+)
 @Composable
-fun GreetingPreview() {
+fun workCircle(){
     Task2Theme {
-        Greeting("Android")
+        Box(
+            modifier = Modifier
+                .padding(0.dp, 25.dp)
+                .size(240.dp,120.dp)
+                .background(Color.Black),
+            contentAlignment = Alignment.TopEnd
+        ){
+            Image(
+                modifier = Modifier,
+                painter = painterResource(R.drawable.circle),
+                contentDescription = "Circle"
+            )
+        }
+
+    }
+}
+
+@Preview(
+    showSystemUi = true,
+    showBackground = true,
+    device = "spec:parent=Galaxy Nexus, orientation=portrait"
+)
+@Composable
+fun modifyWorkCircle(){
+    Task2Theme {
+        Box(
+            modifier = Modifier
+                .padding(0.dp, 25.dp)
+                .size(240.dp,120.dp)
+                .background(Color.Blue),
+            contentAlignment = Alignment.Center
+        ){
+            Image(
+                modifier = Modifier.width(400.dp).fillMaxSize(),
+                contentScale = ContentScale.FillBounds,
+                contentDescription = null,
+                painter = painterResource(R.drawable.circle),
+                colorFilter = ColorFilter.tint(Color(0xFF8B00FF))
+            )
+        }
     }
 }
