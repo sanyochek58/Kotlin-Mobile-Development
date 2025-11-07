ae074fce507afcd93b4e7696f3a93f68c3764c56
sanyochek58 <sanya.burayev.03@mail.ru>
2025-11-07 15:59:19 +0300

    Добавлена реализация задачи 7

--- Task2/app/src/main/java/com/example/task2/MainActivity.kt
+++ Task2/app/src/main/java/com/example/task2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.task2
 
+import android.R
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -7,11 +8,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +29,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -92,53 +101,189 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 // Задача 6
 
+//@Preview(
+//    showBackground = true,
+//    showSystemUi = true,
+//    device = "spec:parent=Galaxy Nexus, orientation=portrait"
+//)
+//@Composable
+//fun workCircle(){
+//    Task2Theme {
+//        Box(
+//            modifier = Modifier
+//                .padding(0.dp, 25.dp)
+//                .size(240.dp,120.dp)
+//                .background(Color.Black),
+//            contentAlignment = Alignment.TopEnd
+//        ){
+//            Image(
+//                modifier = Modifier,
+//                painter = painterResource(R.drawable.circle),
+//                contentDescription = "Circle"
+//            )
+//        }
+//
+//    }
+//}
+//
+//@Preview(
+//    showSystemUi = true,
+//    showBackground = true,
+//    device = "spec:parent=Galaxy Nexus, orientation=portrait"
+//)
+//@Composable
+//fun modifyWorkCircle(){
+//    Task2Theme {
+//        Box(
+//            modifier = Modifier
+//                .padding(0.dp, 25.dp)
+//                .size(240.dp,120.dp)
+//                .background(Color.Blue),
+//            contentAlignment = Alignment.Center
+//        ){
+//            Image(
+//                modifier = Modifier.width(400.dp).fillMaxSize(),
+//                contentScale = ContentScale.FillBounds,
+//                contentDescription = null,
+//                painter = painterResource(R.drawable.circle),
+//                colorFilter = ColorFilter.tint(Color(0xFF8B00FF))
+//            )
+//        }
+//    }
+//}
+
 @Preview(
     showBackground = true,
     showSystemUi = true,
     device = "spec:parent=Galaxy Nexus, orientation=portrait"
 )
 @Composable
-fun workCircle(){
+fun workWithColumn1(){
     Task2Theme {
-        Box(
-            modifier = Modifier
-                .padding(0.dp, 25.dp)
-                .size(240.dp,120.dp)
-                .background(Color.Black),
-            contentAlignment = Alignment.TopEnd
+        Column(
+            modifier = Modifier.padding(0.dp, 25.dp)
         ){
-            Image(
+            Text(
                 modifier = Modifier,
-                painter = painterResource(R.drawable.circle),
-                contentDescription = "Circle"
+                text = "Имя: Евгений"
+            )
+            Text(
+                modifier = Modifier,
+                text = "Отчество: Андреевич"
+            )
+            Text(
+                modifier = Modifier,
+                text = "Фамилия: Лукашин"
+            )
+            Text(
+                modifier = Modifier,
+                text = "Мобильный телефон: +7 495 495 95 95"
+            )
+            Text(
+                modifier = Modifier,
+                text = "г. Москва, 3-я улица Строителей д. 25, кв. 12"
             )
         }
+    }
+}
 
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    device = "spec:parent=Galaxy Nexus, orientation=portrait"
+)
+@Composable
+fun workWithColumn2(){
+    Task2Theme {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                // Первый блок
+                Column(
+                    modifier = Modifier
+                        .padding(25.dp)
+                        .background(color = Color.Gray)
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text("Имя: Евгений")
+                    Text("Отчество: Андреевич")
+                    Text("Фамилия: Лукашин")
+                    Text("Мобильный телефон: +7 495 495 95 95")
+                    Text("Адрес: г. Москва, 3-я улица Строителей д. 25, кв. 12")
+                }
+
+                // Второй блок
+                Column(
+                    modifier = Modifier
+                        .padding(25.dp)
+                        .background(color = Color.Gray)
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text("Имя: Василий")
+                    Text("Отчество: Егорович")
+                    Text("Фамилия: Кузьмин")
+                    Text("Мобильный телефон: - ")
+                    Text("Адрес: г. Москва, 3-я улица Строителей д. 25, кв. 12")
+                }
+            }
+        }
     }
 }
 
 @Preview(
-    showSystemUi = true,
     showBackground = true,
+    showSystemUi = true,
     device = "spec:parent=Galaxy Nexus, orientation=portrait"
 )
 @Composable
-fun modifyWorkCircle(){
+fun workWithColumn3() {
     Task2Theme {
         Box(
-            modifier = Modifier
-                .padding(0.dp, 25.dp)
-                .size(240.dp,120.dp)
-                .background(Color.Blue),
-            contentAlignment = Alignment.Center
-        ){
-            Image(
-                modifier = Modifier.width(400.dp).fillMaxSize(),
-                contentScale = ContentScale.FillBounds,
-                contentDescription = null,
-                painter = painterResource(R.drawable.circle),
-                colorFilter = ColorFilter.tint(Color(0xFF8B00FF))
-            )
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(25.dp)
+                        .background(color = Color.Gray)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(25.dp)
+                            .weight(1f),
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Text("Имя: Евгений")
+                        Text("Отчество: Андреевич")
+                        Text("Фамилия: Лукашин")
+                        Text("Мобильный телефон: +7 495 495 95 95")
+                        Text("Адрес: г. Москва, 3-я улица Строителей д. 25, кв. 12")
+                    }
+
+                    Image(
+                        painter = painterResource(R.drawable.star_on),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(40.dp)
+                            .padding(end = 16.dp)
+                    )
+                }
+            }
         }
     }
 }
