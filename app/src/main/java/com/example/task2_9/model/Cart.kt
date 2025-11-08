b006650ed5ef745f6b2d71008f3833b1cdf98e5a
Alexandr Buraev <148450042+sanyochek58@users.noreply.github.com>
2025-11-08 18:33:17 +0300

    Delete app directory

--- app/src/main/java/com/example/task2_9/model/Cart.kt
+++
@@ -1,7 +0,0 @@
-package com.example.task2_9.model
-
-data class Cart(
-    val products: List<Product>
-){
-    val total: Int get() = products.sumOf { it.price }
-}
--- app/src/main/java/com/example/task2_9/model/Product.kt
+++
@@ -1,7 +0,0 @@
-package com.example.task2_9.model
-
-data class Product(
-    val id: Int,
-    val name: String,
-    val price: Int
-)
